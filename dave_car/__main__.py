1a251ef1f8bb862f78015e381b46f221c1e15d09
Graylan <34530588+graylan0@users.noreply.github.com>
2023-09-06 12:37:51 -0400

    Update __main__.py

--- dave_car/__main__.py
+++ dave_car/__main__.py
@@ -4,15 +4,29 @@ from fastapi.logger import logger
 from fastapi.exceptions import RequestValidationError
 import logging
 import asyncio
-from edge_detection_ocr import edge_detection_ocr
-from stable_diffusion import simulate_pathways
-from camera_input import capture_image
-from llama2_control import control_vehicle_with_llama2
+import cv2
+import json
+import uuid
+from edge_detection_ocr import edge_detection_ocr  # Assuming this function is in another file
 
 # Initialize FastAPI and Logging
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 task_queue = asyncio.Queue()
+
+def convert_to_ascii(edges):
+    ascii_art = ""
+    step_size_row = edges.shape[0] // 50
+    step_size_col = edges.shape[1] // 10
+
+    for i in range(0, edges.shape[0], step_size_row):
+        for j in range(0, edges.shape[1], step_size_col):
+            if edges[i, j] > 128:
+                ascii_art += "X"
+            else:
+                ascii_art += " "
+        ascii_art += "\n"
+    return ascii_art
 
 async def execute_cycle():
     while True:
@@ -29,11 +43,16 @@ async def execute_cycle():
                 logger.error("Edge detection or OCR failed.")
                 continue
             
-            # Simulate pathways using stable diffusion
-            simulated_pathways = await simulate_pathways(edges)
+            # Convert edges to ASCII art
+            ascii_art = convert_to_ascii(edges)
+            
+            # Save ASCII art to a .json file with a randomized title
+            random_title = str(uuid.uuid4()) + ".json"
+            with open(random_title, "w") as f:
+                json.dump({"ascii_art": ascii_art, "ocr_text": ocr_text}, f)
             
             # Control the vehicle using Llama2
-            command = await control_vehicle_with_llama2(edges, ocr_text, simulated_pathways)
+            command = await control_vehicle_with_llama2(edges, ocr_text, ascii_art)
             
             # Send `command` to your vehicle control system
             logger.info(f"Command sent to vehicle: {command}")
@@ -57,3 +76,4 @@ async def validation_exception_handler(request, exc):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
